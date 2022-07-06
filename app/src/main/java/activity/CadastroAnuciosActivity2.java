5a3c505158ab3f2cb9b9bfc17521cfb5592b9dd8
ryanl <ryanlimadev@gmail.com>
2022-07-06 12:04:13 -0300

    formatando moedas

--- app/src/main/java/activity/CadastroAnuciosActivity2.java
+++ app/src/main/java/activity/CadastroAnuciosActivity2.java
@@ -3,14 +3,38 @@ package activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
+import com.blackcat.currencyedittext.CurrencyEditText;
 import com.rlds.olxclone.R;
 
+import java.util.Currency;
+import java.util.Locale;
+
 public class CadastroAnuciosActivity2 extends AppCompatActivity {
+    private EditText campoTitulo, campoDescricao;
+    private CurrencyEditText campoValor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_anucios2);
+        // configurações iniciais
+        campoDescricao = findViewById(R.id.editDescricao);
+        campoTitulo = findViewById(R.id.editTitulo);
+        campoValor = findViewById(R.id.edittextValor);
+        // configurar localidade para portugues pt -Br
+        Locale locale = new Locale( "pt","BR");
+        campoValor.setLocale(locale);
+
+
+
+    }
+    public  void salvarAnucio(View view){
+        Long valor = campoValor.getRawValue();
+        Log.d("valor","salvar anucio" + valor);
+
     }
 }
