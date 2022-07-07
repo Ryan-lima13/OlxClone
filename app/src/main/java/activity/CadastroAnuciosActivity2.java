854f9fbe1192f9a9ce2ab14f708af6976539f6f9
ryanl <ryanlimadev@gmail.com>
2022-07-07 11:42:15 -0300

    escolhendo fotos  do anucios

--- app/src/main/java/activity/CadastroAnuciosActivity2.java
+++ app/src/main/java/activity/CadastroAnuciosActivity2.java
@@ -1,34 +1,41 @@
 package activity;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
-import android.app.DatePickerDialog;
+import android.app.Activity;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.blackcat.currencyedittext.CurrencyEditText;
 import com.rlds.olxclone.R;
 import com.santalu.maskara.widget.MaskEditText;
 
-import java.util.Currency;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 import helper.Permissoes;
 
-public class CadastroAnuciosActivity2 extends AppCompatActivity {
+public class CadastroAnuciosActivity2 extends AppCompatActivity implements View.OnClickListener {
     private EditText campoTitulo, campoDescricao;
     private CurrencyEditText campoValor;
     private MaskEditText campoTelefone;
+    private ImageView imageView1,imageView2,imageView3;
     private  String[] permissoes = new String[]{
             Manifest.permission.READ_EXTERNAL_STORAGE
     };
+    private List<String> listaFotosREcuperadas = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,10 +53,60 @@ public class CadastroAnuciosActivity2 extends AppCompatActivity {
         // configurar localidade para portugues pt -Br
         Locale locale = new Locale( "pt","BR");
         campoValor.setLocale(locale);
+        imageView1 = findViewById(R.id.imageCadastro1);
+        imageView2 = findViewById(R.id.imageCadastro2);
+        imageView3 = findViewById(R.id.imageCadastro3);
+        imageView1.setOnClickListener(this);
+        imageView2.setOnClickListener(this);
+        imageView3.setOnClickListener(this);
 
 
 
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.imageCadastro1:
+                escolherImagem(1);
+                break;
+            case R.id.imageCadastro2:
+                escolherImagem(2);
+                break;
+            case R.id.imageCadastro3:
+                escolherImagem(3);
+                break;
+
+        }
+    }
+
+    public  void escolherImagem(int requestCode){
+        Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(i,requestCode);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == Activity.RESULT_OK){
+            // Recuperar imagem
+            Uri imagemSelecionada = data.getData();
+            String caminhoImagem = imagemSelecionada.toString();
+            // configurar imagem no ImageView
+            if(requestCode ==1){
+                imageView1.setImageURI(imagemSelecionada);
+
+            }else if(requestCode == 2){
+                imageView2.setImageURI(imagemSelecionada);
+            }else if(resultCode == 3){
+                imageView3.setImageURI(imagemSelecionada);
+            }
+            listaFotosREcuperadas.add(caminhoImagem);
+
+
+        }
     }
 
     @Override
